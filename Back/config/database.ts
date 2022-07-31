02fa9bef83e72efcb35e397c89235995d3f9e8d0
Keli Booster <kbooster17@gmail.com>
2022-07-31 18:36:01 +0000

    setup postgres

--- Back/config/database.ts
+++ Back/config/database.ts
@@ -1,11 +1,19 @@
-import path from 'path';
-
-export default ({ env }) => ({
+require("dotenv").config();
+module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", process.env.DBNAME),
+      user: env("DATABASE_USERNAME", process.env.DBUSER),
+      password: env("DATABASE_PASSWORD", process.env.DBPASSWORD),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      // },
+      ssl: false,
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
--- Back/dist/config/database.js
+++ Back/dist/config/database.js
@@ -1,15 +1,19 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
-exports.default = ({ env }) => ({
+require("dotenv").config();
+module.exports = ({ env }) => ({
     connection: {
-        client: 'sqlite',
+        client: "postgres",
         connection: {
-            filename: path_1.default.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+            host: env("DATABASE_HOST", "127.0.0.1"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", process.env.DBNAME),
+            user: env("DATABASE_USERNAME", process.env.DBUSER),
+            password: env("DATABASE_PASSWORD", process.env.DBPASSWORD),
+            schema: env("DATABASE_SCHEMA", "public"),
+            // ssl: {
+            //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+            // },
+            ssl: false,
         },
-        useNullAsDefault: true,
+        debug: false,
     },
 });
