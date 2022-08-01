4ff0beca6e1d3bd091e20101c3e183ce1278cc6b
Keli Booster <kbooster17@gmail.com>
2022-08-01 15:18:35 +0000

    learn strapi

--- Back/dist/src/api/post/controllers/post.js
+++ Back/dist/src/api/post/controllers/post.js
@@ -4,4 +4,29 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::post.post');
+exports.default = strapi_1.factories.createCoreController("api::post.post", ({ strapi }) => ({
+    async find(ctx) {
+        // Calling the default core action
+        const { data, meta } = await super.find(ctx);
+        const query = strapi.db.query("api::post.post");
+        await Promise.all(data.map(async (item, index) => {
+            const post = await query.findOne({
+                where: {
+                    id: item.id,
+                },
+                populate: ["createdBy", "updatedBy"],
+            });
+            data[index].attributes.createdBy = {
+                id: post.createdBy.id,
+                firstname: post.createdBy.firstname,
+                lastname: post.createdBy.lastname,
+            };
+            data[index].attributes.updatedBy = {
+                id: post.updatedBy.id,
+                firstname: post.updatedBy.firstname,
+                lastname: post.updatedBy.lastname,
+            };
+        }));
+        return { data, meta };
+    },
+}));
--- Back/src/api/post/controllers/post.ts
+++ Back/src/api/post/controllers/post.ts
@@ -2,6 +2,40 @@
  *  post controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController(
+  "api::post.post",
+  ({ strapi }) => ({
+    async find(ctx) {
+      // Calling the default core action
+      const { data, meta } = await super.find(ctx);
+
+      const query = strapi.db.query("api::post.post");
+
+      await Promise.all(
+        data.map(async (item, index) => {
+          const post = await query.findOne({
+            where: {
+              id: item.id,
+            },
+            populate: ["createdBy", "updatedBy"],
+          });
+
+          data[index].attributes.createdBy = {
+            id: post.createdBy.id,
+            firstname: post.createdBy.firstname,
+            lastname: post.createdBy.lastname,
+          };
+          data[index].attributes.updatedBy = {
+            id: post.updatedBy.id,
+            firstname: post.updatedBy.firstname,
+            lastname: post.updatedBy.lastname,
+          };
+        })
+      );
+
+      return { data, meta };
+    },
+  })
+);
